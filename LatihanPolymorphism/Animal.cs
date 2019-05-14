db2871e8a36957542974d54ead3bf0ebc78708c0
fachrizalrd <fachrizal.deninsyah@students.amikom.ac.id>
2019-05-14 14:41:22 +0700

    first commit

---
+++ LatihanPolymorphism/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism
+{
+    public class Animal
+    {
+        public string Name { set; get; }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("The Animal Speaks");
+        }
+    }
+}
---
+++ LatihanPolymorphism/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Speak();
+
+            Cat cat = new Cat();
+            animal = cat;
+            animal.Speak();
+
+            Chicken chicken = new Chicken();
+            animal = chicken;
+            animal.Speak();
+
+            Dog dog = new Dog();
+            animal = dog;
+            animal.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
